6216385d45338f1efea1b516395f2785e74c4623
Emediong Bassey Edem <edememediong1@gmail.com>
2025-02-11 18:08:30 +0100

    buidling the TicketSelection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./Components/Primary Components/Navbar"
+import TicketSelection from "./Components/Primary Components/TicketSelection"
+
 
 function App() {
   return(
-    <div className="body h-screen flex place-content-center">
+    <div className="body h-screen flex flex-col justify-start items-center gap-10">
         <Navbar />
+        <TicketSelection/>
     </div>
   )
 }
---
+++ src/Components/Primary Components/TicketSelection.jsx
@@ -0,0 +1,27 @@
+
+
+function TicketSelection() {
+  return (
+    <div className="w-[85%] border-[#197686] border-[1px] md:w-[50%]">
+        <section className="">
+            <div className="text-white">
+                <h1>Ticket Selection</h1>
+                <p>Step 1/3</p>
+            </div>
+            <div></div>
+        </section>
+        <article>
+            <div>
+                <h1>Techember Fest &apos; 25</h1>
+                <p>Join us for an unforgettable experience at Techember Fest! Secure your spot now!</p>
+                <p>📍 Daily Times Hall, Abuja || March 15th, 2025 || 9PM</p>
+            </div>
+
+            <hr className="bg-[#07373F] h-[4px]"/>
+
+        </article>
+    </div>
+  )
+}
+
+export default TicketSelection
