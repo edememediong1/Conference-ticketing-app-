a36bcb7d6fd6f2974973cda5492ff2f057162dcd
Emediong Bassey Edem <edememediong1@gmail.com>
2025-02-13 11:24:17 +0100

    I will fix the image upload soon

--- src/Components/Primary Components/TicketForm.jsx
+++ src/Components/Primary Components/TicketForm.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 function TicketForm({userDetails, setUserDetails, onNext, onBack}) {
-  return (
+    
+    const handleImageChange = (e) => {
+        const file = e.target.files[0]
+        if (file) {
+          const reader = new FileReader()
+          reader.onloadend = () => {
+            setUserDetails((prev) => ({ ...prev, image: reader.result }))
+          }
+          reader.readAsDataURL(file)
+        }
+      }
+    
+  
+    return (
     <div className="w-[90%] bg-[#08252B] ms:bg-[#041E23] p-[24px] border-[#197686] border-[1px] ms:w-[60%] md:w-[60%] md:p-[48px] rounded-2xl space-y-6">
         <section className="flex-col justify-center items-center space-y-3">
             <div className="text-white md:flex md:justify-between md:items-center">
@@ -12,7 +25,24 @@ function TicketForm({userDetails, setUserDetails, onNext, onBack}) {
         </section>
         <article className="space-y-6 bg-[#08252B] ms:p-[24px] ms:border-[1px] ms:border-[#197686] ms:rounded-[24px]">
             <section>
-                
+                <p className='text-white mb-2'>Upload profile photo</p>
+                <div className="bg-[#000] w-full h-auto flex justify-center items-center rounded-[12px]">
+                    <div className='w-[240px] h-[240px] bg-[#0E464F] rounded-[12px]'>
+                    <div>
+                        {/* <label htmlFor="image" className="block text-sm font-medium text-gray-700">
+                            Upload Image
+                        </label> */}
+                        <input
+                            type="file"
+                            id="image"
+                            name="image"
+                            accept="image/*"
+                            onChange={handleImageChange}
+                            className="mt-1 block w-full"
+                        />
+                        </div>
+                    </div>
+                </div>
             </section>
 
         </article>
