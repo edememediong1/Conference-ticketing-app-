052d88f1c038dd3f61c4daec65547743b5ba8d51
Emediong Bassey Edem <edememediong1@gmail.com>
2025-02-13 15:26:55 +0100

    basic structure of preview #1

--- src/Components/Primary Components/TicketView.jsx
+++ src/Components/Primary Components/TicketView.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+// import React from 'react'
 
 function TicketView() {
   return (
-    <div className=" w-[90%] bg-[#08252B] ms:bg-[#041E23] p-[24px] border-[#197686] border-[1px] ms:w-[60%] md:w-[55%] md:p-[48px] rounded-2xl space-y-6">
+    <div className=" w-[90%] bg-[#08252B] ms:bg-[#041E23] p-[24px] border-[#197686] border-[1px] ms:w-[70%] md:w-[55%] md:p-[48px] rounded-2xl space-y-6">
         <section className="flex-col justify-center items-center space-y-3">
             <div className="text-white md:flex md:justify-between md:items-center">
                 <h1 className="text-[24px] md:text-[32px]">Attendee Details</h1>
@@ -16,8 +16,46 @@ function TicketView() {
             <p>Check your email for a copy or you can <strong>download</strong></p>
         </section>
 
-        <section className='card h-50 border-[#197686] border-[1px] w-[70%] m-auto' >
-            Body
+        <section className='card border-[#197686] border-[1px] w-[400px] ms:w-[80%] m-auto h-[600px]' >
+            <div>
+                <div className="w-full text-white">
+                    <div className="border-[1px] border-[#197686] flex flex-col justify-center items-center">
+                        <h1>Techember Fest&apos;25</h1>
+                        <p>📍 04 Rumens road, Ikoyi, Lagos</p>
+                        <p>📅 March 15, 2025 | 7:00 PM</p>
+
+                        <div className="w-[140px] h-[140px] border-[4px] border-[#197686] ">
+                            <img src="" alt="image display" />
+                        </div>
+
+                        <div>
+                            <div>
+                                <p>Enter your name</p>
+                                <p></p>
+                            </div>
+                            <div>
+                                <p>Enter your email</p>
+                                <p></p>
+                            </div>
+                            <div>
+                                <p>Ticket Type</p>
+                                <p></p>
+                            </div>
+                            <div>
+                                <p>Ticket for</p>
+                                <p></p>
+                            </div>
+                            <div>
+                                <p>Special request?</p>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                </div>
+
+            </div>
         </section>
 
     </div>
